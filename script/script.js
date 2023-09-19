6ebd800ccd56dccae354a877f4d4e3eb9bb01ef3
Marcone Santos <98716688+Marcone-Santos1@users.noreply.github.com>
2023-09-18 22:02:56 -0300

    (feat): add images and email send communication with api

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 window.addEventListener("scroll", function () {
-  var header = document.querySelector("header");
+  let header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
 });
 
@@ -17,18 +17,73 @@ btnMobile.addEventListener("touchstart", toggleMenu);
 
 /* pegar dados do form */
 
-// var form = document.querySelector("#form");
-// var nome = document.querySelector("#nome");
-// var tel = document.querySelector("#telefone");
-// var email = document.querySelector("#email");
+let form = document.querySelector("#form");
+let nome = document.querySelector("#nome");
+let tel = document.querySelector("#telefone");
+let email = document.querySelector("#email");
+let description = document.querySelector("#description");
+let button = document.getElementById("formButton");
 
-// form.addEventListener("submit", (e) => {
-//   e.preventDefault();
-// })
+const sendMail = async (messageContent) => {
+  const request = await fetch('http://127.0.0.1:8000/send-mail', {
+    method: 'POST',
+    headers: {
+      "Content-type": "application/json"
+    },
+    body: JSON.stringify(messageContent),
+  })
 
-// function mostrar(){
-//   alert(nome.value)
-//   alert(tel.value)
-//   alert(email.value)
-// }
+  console.log(request)
 
+  const response = await request.json()
+
+  console.log(response)
+}
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const messageContent = {
+    name: nome.value,
+    phone: tel.value,
+    mail: email.value,
+    description: description.value,
+  }  
+  await loadingButton(button, true);
+  await clearOrcamentoInputs(messageContent);
+  await sendMail(messageContent)
+  await loadingButton(button);
+})
+
+const loadingButton = async (button, active = false) => {
+
+  await showSuccessMessage(active);
+
+  if (active) {
+    button.innerHTML = '<i class="fa fa-spinner fa-spin"></i>'
+    button.setAttribute('disabled', 'disabled')
+    return
+  }
+
+  button.innerHTML = 'Enviar'
+  button.removeAttribute('disabled')
+}
+
+const clearOrcamentoInputs = async () => {
+  nome.value = ''
+  tel.value = ''
+  email.value = ''
+  description.value = ''
+}
+
+const showSuccessMessage = async (active = false) => {
+  const successMessage = document.getElementById("successMessage")
+
+  if (active) {
+    console.log('teste')
+    successMessage.style.display = "block"
+    return
+  }
+  successMessage.style.display = "none"
+
+}
